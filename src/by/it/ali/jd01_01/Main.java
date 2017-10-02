f19283b768b1125bb9a00b40e578a7427109b118
Ahremenko <a.hremenko@gmail.com>
2017-10-02 11:59:15 +0300

    Test

---
+++ src/by/it/ali/jd01_01/Main.java
@@ -0,0 +1,10 @@
+package by.it.ali.jd01_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MFC fC = new MFC();
+        fC.setname("Hello, world!");
+        fC.printname();
+    }
+}
