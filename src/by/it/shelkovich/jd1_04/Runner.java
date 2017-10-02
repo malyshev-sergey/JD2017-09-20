3cb46242b73ecaeaf2c30ae05bd2ed4749eae17c
Anton Shelkovich <anton.shelkovich@gmail.com>
2017-10-02 10:28:15 +0300

    второе и четвертое задание

---
+++ src/by/it/shelkovich/jd1_04/Runner.java
@@ -0,0 +1,16 @@
+package by.it.shelkovich.jd1_04;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        //Задание А
+        TaskA.run();
+
+        //Задание В
+        TaskB taskB = new TaskB();
+        taskB.printSummary();
+
+        //Задание С
+        TaskC.run();
+    }
+}
---
+++ src/by/it/shelkovich/jd1_04/Util.java
@@ -0,0 +1,127 @@
+package by.it.shelkovich.jd1_04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Util {
+
+    /**
+     * Читает строку из консоли
+     * @return Прочитанную строку
+     */
+    static String readLine() {
+        InputStreamReader isReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(isReader);
+        String line = null;
+        try {
+            line = bufferedReader.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return line;
+    }
+
+    /**
+     * Поиск минимального значения в одномерном массиве
+     * @param array одномерный массив
+     * @return минимальное значение
+     */
+    static double findMinDouble(double[] array) {
+        double min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) min = array[i];
+        }
+        return min;
+    }
+
+    /**
+     * Поиск максимального значения в одномерном массиве
+     * @param array одномерный массив
+     * @return максимальное значение
+     */
+    static double findMaxDouble(double[] array) {
+        double max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) max = array[i];
+        }
+        return max;
+    }
+
+    /**
+     * Сортирует массив методом слияния
+     *
+     * @param array Массив, который нужно отсортировать
+     */
+    static void mergeSort(double[] array) {
+        mergeSort(array, 0, array.length - 1, true);
+    }
+
+    /**
+     * Сортирует массив методом слияния
+     *
+     * @param array       Массив, который нужно отсортировать
+     * @param upDirection Направление сортировки (true - по возрастанию)
+     */
+    static void mergeSort(double[] array, boolean upDirection) {
+        mergeSort(array, 0, array.length - 1, upDirection);
+    }
+
+    private static void mergeSort(double[] sourceArray, int startPos, int endPos, boolean upDirection) {
+
+        if (endPos <= startPos) return;
+        int mid = startPos + (endPos - startPos) / 2;
+        mergeSort(sourceArray, startPos, mid, upDirection);
+        mergeSort(sourceArray, mid + 1, endPos, upDirection);
+
+        double[] first = new double[mid - startPos + 1];
+        System.arraycopy(sourceArray, startPos, first, 0, first.length);
+
+        double[] second = new double[endPos - (mid + 1) + 1];
+        System.arraycopy(sourceArray, mid + 1, second, 0, second.length);
+
+        double[] result = new double[first.length + second.length];
+        int fI = 0;
+        int sI = 0;
+        int rI = 0;
+        boolean condition;
+
+        while (fI < first.length && sI < second.length) {
+            if (upDirection) condition = first[fI] < second[sI];
+            else condition = first[fI] > second[sI];
+
+            if (condition) {
+                result[rI] = first[fI];
+                fI++;
+            } else {
+                result[rI] = second[sI];
+                sI++;
+            }
+            rI++;
+        }
+
+        if (fI == first.length) System.arraycopy(second, sI, result, rI, second.length - sI);
+        else if (sI == second.length) System.arraycopy(first, fI, result, rI, first.length - fI);
+
+        System.arraycopy(result, 0, sourceArray, startPos, result.length);
+
+    }
+
+    static int binarySearch(double[] array, double toFind){
+        int leftPos = 0;
+        int rightPos = array.length;
+
+        while(leftPos<=rightPos){
+            int midPos = (rightPos+leftPos)/2;
+            if(toFind == array[midPos]) return midPos;
+            else{
+                if(toFind < array[midPos]) rightPos = midPos-1;
+                else leftPos = midPos+1;
+            }
+        }
+
+        return -1;
+    }
+
+
+}
