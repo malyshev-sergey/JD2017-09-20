ab2304f2fe14f842b70143a921307537f696443b
Anton Shelkovich <anton.shelkovich@gmail.com>
2017-10-02 12:32:00 +0300

    Задание А пятого занятия

---
+++ src/by/it/shelkovich/jd1_05/Runner.java
@@ -0,0 +1,10 @@
+package by.it.shelkovich.jd1_05;
+
+public class Runner {
+    public static void main(String[] args) {
+        TaskA.one();
+        TaskA.two();
+
+        TaskA.three();
+    }
+}
---
+++ src/by/it/shelkovich/jd1_05/Util.java
@@ -0,0 +1,18 @@
+package by.it.shelkovich.jd1_05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Util {
+
+    static String readLine(){
+        InputStreamReader isReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(isReader);
+        String line = null;
+        try {
+            line = bufferedReader.readLine();
+        } catch (Exception e){e.printStackTrace();}
+
+        return line;
+    }
+}
