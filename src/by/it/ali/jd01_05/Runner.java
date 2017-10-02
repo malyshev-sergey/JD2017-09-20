91fd3546e06a53bdc4a4b937e456068cb5e98e4b
AliAbbas29 <AliAbbas2905@gmail.com>
2017-10-02 23:03:37 +0300

    Done B-tasks

--- src/by/it/ali/jd01_05/Runner.java
+++ src/by/it/ali/jd01_05/Runner.java
@@ -2,8 +2,10 @@ package by.it.ali.jd01_05;
 
 public class Runner {
     public static void main(String[] args) {
-        TaskA.SolveA1();
-        TaskA.SolveA2();
-        TaskA.SolveA3();
+//        TaskA.SolveA1();
+//        TaskA.SolveA2();
+//        TaskA.SolveA3();
+        TaskB.SolveB1();
+        TaskB.SolveB2();
     }
 }
