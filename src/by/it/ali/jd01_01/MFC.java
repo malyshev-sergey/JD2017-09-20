3f19796d693f580bfed5d104c0c1e2d0af1edc03
Ahremenko <a.hremenko@gmail.com>
2017-10-02 11:59:15 +0300

    Test

---
+++ src/by/it/ali/jd01_01/MFC.java
@@ -0,0 +1,8 @@
+package by.it.ali.jd01_01;
+
+public class MFC {
+   private String name="Hello world!";
+   public void setname(String name) {this.name = name ;}
+   void printname() { System.out.println(this.name); }
+
+}
