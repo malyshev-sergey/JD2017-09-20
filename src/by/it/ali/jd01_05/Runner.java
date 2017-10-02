0de83e4c88306bd61d46e47f9dbdbdaf6cf410c9
Ahremenko <a.hremenko@gmail.com>
2017-10-02 12:30:12 +0300

    Done jd01_05 TaskA

---
+++ src/by/it/ali/jd01_05/Runner.java
@@ -0,0 +1,12 @@
+package by.it.ali.jd01_05;
+
+/**
+ * Created by user on 02.10.2017.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        TaskA.SolveA1();
+        TaskA.SolveA2();
+        TaskA.SolveA3();
+    }
+}
