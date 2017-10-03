f1727af3dbb029b9e3b5a44d67c09d605edaee8c
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-03 19:24:02 +0300

    02-04 v1.1

--- src/by/it/malyshev/jd01_03/Runner.java
+++ src/by/it/malyshev/jd01_03/Runner.java
@@ -33,24 +33,39 @@ public class Runner {
         InOut.printArray(oneDimDoubleArray);
         System.out.println();
 
+        System.out.println("Умножение матрицы на вектор");
         double[][] matrix1 = {{2, 5, 4, 1},
                 {1, 3, 2, 1},
                 {2, 10, 9, 7},
                 {3, 8, 9, 2}};
+        System.out.println("\nИсходная матрица");
+        InOut.print2DArray(matrix1);
         double[] vector = {1, -2, 56, -45};
+        System.out.println("\nИсходный вектор");
+        InOut.printArray(vector);
         if (matrix1[0].length == vector.length) {
             double[] product = Utils.matrixVectorProduct(matrix1, vector);
+            System.out.println("\nПродукт");
             InOut.printArray(product);
         } else
             System.out.println("Число столбцов в матрице не равно\nс числу строк в векторе-столбце");
 
         System.out.println();
+
+        System.out.println("Умножение матрицы на матрицу");
+        System.out.println("\nИсходная матрица A");
+        InOut.print2DArray(matrix1);
         double[][] matrix2 = {{2, 5, 4, 1},
                 {1, 3, 2, 1},
                 {2, 10, 9, 7},
                 {3, 8, 9, 2}};
+        System.out.println("\nИсходная матрица B");
+        InOut.print2DArray(matrix2);
+
         if (matrix1[0].length == matrix2.length) {
             double[][] product2D = Utils.matrixesProduct(matrix1, matrix2);
+            System.out.println("\nПродукт");
+
             InOut.print2DArray(product2D);
         } else
             System.out.println("Число столбцов в матрице A не равно\nс числу строк в матрице B");
