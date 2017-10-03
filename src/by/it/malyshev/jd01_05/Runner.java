d1cdc8ba258d7a1a23f4298585911ce4bcae2d66
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-04 01:17:23 +0300

    05 A-B solved

---
+++ src/by/it/malyshev/jd01_05/Runner.java
@@ -0,0 +1,24 @@
+package by.it.malyshev.jd01_05;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nЗадание№1\n");
+        TaskA.solveTaskA1();
+
+        System.out.println("\nЗадание№2\n");
+        System.out.println("ввести a=1.21");
+        TaskA.solveTaskA2();
+
+        System.out.println("\nЗадание№3\n");
+        TaskA.sovleTaskA3();
+
+        System.out.println("\nЗадание№4\n");
+        TaskB.functionFour(1, 8, 1);
+
+        System.out.println("\nЗадание№5\n");
+        TaskB.functionFive(-2, 15, 1.5, 0.491E4);
+
+    }
+}
