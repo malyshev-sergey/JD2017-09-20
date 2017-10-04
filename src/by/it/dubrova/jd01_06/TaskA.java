b059a1b52c9ada0481987c97cb97e621f0739cd3
Dubrova <maksim.dubrova@gmail.com>
2017-10-04 18:00:09 +0300

    Первый коммит

--- src/by/it/dubrova/jd01_06/TaskA.java
+++ src/by/it/dubrova/jd01_06/TaskA.java
@@ -1,5 +1,10 @@
 package by.it.dubrova.jd01_06;
 
 public class TaskA {
+    public static void main(String[] args) {
 
+        StringBuilder sb = new StringBuilder(Data.lukomor);
+
+        
+    }
 }
