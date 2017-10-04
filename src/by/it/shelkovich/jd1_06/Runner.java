247c3988af24d919760d4521b87bdafa2d48e88c
Anton Shelkovich <anton.shelkovich@gmail.com>
2017-10-04 20:04:40 +0300

    Задания А и B шестого занятия

---
+++ src/by/it/shelkovich/jd1_06/Runner.java
@@ -0,0 +1,11 @@
+package by.it.shelkovich.jd1_06;
+
+public class Runner {
+    public static void main(String[] args) {
+        //TaskA.part1();
+        //TaskA.part2();
+
+        //TaskB.part1();
+        TaskB.part2();
+    }
+}
