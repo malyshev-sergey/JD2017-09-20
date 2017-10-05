c1de7ea53f620452e943df0aa96cf80d131b1aed
Dubrova <maksim.dubrova@gmail.com>
2017-10-05 20:08:23 +0300

    Всё работает! Ура постарался сделать без варнингов

--- src/by/it/dubrova/jd01_03/InOutA.java
+++ src/by/it/dubrova/jd01_03/InOutA.java
@@ -17,28 +17,35 @@ package by.it.dubrova.jd01_03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class InOutA {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { // везде ли нужно указывать throws и зачем и что это?
 
-
-        double[][] doubleArray = create2DArrayFromString("1 3 4 5 2 5 6 8", "1 2 4 5 6 7 8 9", "5 7 3 7 9 7 4 6");
-
-        System.out.println("dddd");
+        System.out.println("jd01_03 постарался сделать аккуратно, с соблюдением правил написания кода");
+        System.out.print("Введи строку с клавиатуры - double через пробел, и я создам из неё массив:");
+        String s = inputFromKeyboard();
+        System.out.println("\r\nВведена строка: " + s);
+        System.out.println("Метод createArrayFromString создал из неё одномерный массив! Вот элемент с индексом 0: " + createArrayFromString(s)[0]);
+        System.out.print("\r\nА сейчас создадим двумерный! Введите массив добл через пробел для первой мерности:");
+        String mer1 = inputFromKeyboard();
+        System.out.print("\r\nВведите массив добл через пробел для второй мерности:");
+        String mer2 = inputFromKeyboard();
+        System.out.println("\r\nЭлемент [0][0] вот:" + create2DArrayFromString(mer1,mer2)[0][0]);
+        System.out.println("А вот Макс и Мин значения с первого введенного массива: "
+                + Util.getMinValueFromArray(createArrayFromString(s)) + " и " + Util.getMaxValueFromArray(createArrayFromString(s)));
     }
 
 
 
 
-public static String inputFromKeyboard() throws IOException {
+private static String inputFromKeyboard() throws IOException {
     BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
     return b.readLine();
 }
 
 
 
-public static double[] createArrayFromString(String s){
+private static double[] createArrayFromString(String s){
     String[] tmp = s.split(" ");
     double[] arr = new double[tmp.length];
 
@@ -49,7 +56,7 @@ public static double[] createArrayFromString(String s){
     return arr;
 }
 
-public static double[][] create2DArrayFromString(String... s){
+private static double[][] create2DArrayFromString(String... s){
     int countRow = s.length; //длинна первой мерности
 
     double d[][] = new double[countRow][];
@@ -57,16 +64,20 @@ public static double[][] create2DArrayFromString(String... s){
         //d[i][j]
     for (int i = 0; i < countRow; i++){  //тут заполняем 1 мерность
 
-        double tmp[] = createArrayFromString(s[i]); //присвоили ссылку на массив
+        //double tmp[] = createArrayFromString(s[i]); //присвоили ссылку на массив
+        double tmp[] = createArrayFromString(s[i]).clone(); // методом клон
         int slen = tmp.length;
-        for (int j = 0; j < slen; j++){// 2 мерность
-            d[i] = new double[slen];  // создание новго элемента, пздц
-            d[i][j] = tmp[j];
-//System.out.print(" " + tmp[j]);
-        }
+        d[i] = new double[slen]; // создание новго элемента, пздц
+
+        d[i] = tmp.clone();// этот метод можно использовать в обход циклов! И варнинги не вылазят
+
+        //for (int j = 0; j < slen; j++){// 2 мерность
+        //    d[i][j] = tmp[j];
+//System.out.print(" " + d[i][j]);
+        //}
 //System.out.println();
     }
-
+// правильно ли происходит присвоение ссылки на двумерный массив???
     return d;
 }
 
