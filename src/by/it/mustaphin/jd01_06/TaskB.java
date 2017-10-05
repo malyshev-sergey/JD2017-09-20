ad8cb67c1bb0edeb694cf458475af8d1d2b3b559
marat <pcgeniusweb@gmail.com>
2017-10-05 12:05:27 +0300

    TaskB

--- src/by/it/mustaphin/jd01_06/TaskB.java
+++ src/by/it/mustaphin/jd01_06/TaskB.java
@@ -1,4 +1,16 @@
 package by.it.mustaphin.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskB {
+
+    void findVowel(String text) {
+        Pattern pattern = Pattern.compile("");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+
+        }
+    }
+
 }
