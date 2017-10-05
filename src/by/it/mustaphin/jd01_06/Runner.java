94c023fe4a77bb93a3be477e04de89ae3535a9c3
marat <pcgeniusweb@gmail.com>
2017-10-05 11:16:06 +0300

    TaskB

--- src/by/it/mustaphin/jd01_06/Runner.java
+++ src/by/it/mustaphin/jd01_06/Runner.java
@@ -3,7 +3,9 @@ package by.it.mustaphin.jd01_06;
 public class Runner {
 
     public static void main(String[] args) {
-        TaskA1 tA1 = new TaskA1();
-        tA1.replaceSymbs();
+//        TaskA1 tA1 = new TaskA1();
+//        tA1.replaceSymbs();
+        TaskA2 tA2 = new TaskA2();
+        tA2.countLetter(Util.TXT);
     }
 }
--- src/by/it/mustaphin/jd01_06/TaskA2.java
+++ src/by/it/mustaphin/jd01_06/TaskA2.java
@@ -1,10 +1,22 @@
 package by.it.mustaphin.jd01_06;
 
-public class TaskA2 {
+class TaskA2 {
 
-    void fingGlassnye() {
-
-
+    void countLetter(String text) {
+        String[] textArr = text.split("[^а-яА-ЯёЁ]+");
+        for (String word : textArr) {
+            if (word.isEmpty()) {
+                continue;
+            }
+            int count = 1;
+            for (int i = 0; i < textArr.length; i++) {
+                if (word.equalsIgnoreCase(textArr[i])) {
+                    count++;
+                    textArr[i] = "";
+                }
+            }
+            System.out.printf("%-12s : %-2d\n", word, --count);
+        }
     }
 
 }
