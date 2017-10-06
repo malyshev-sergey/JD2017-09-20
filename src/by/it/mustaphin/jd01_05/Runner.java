fff5c202b0e787924c0cf13e1adba26c8dafc2df
marat <pcgeniusweb.mm@gmail.com>
2017-10-06 19:24:35 +0300

    Сделал jd01_05 TaskB2

---
+++ src/by/it/mustaphin/jd01_05/Runner.java
@@ -0,0 +1,12 @@
+package by.it.mustaphin.jd01_05;
+
+public class Runner {
+
+    public static void main(String[] args) {
+//        TaskB1 tB1 = new TaskB1();
+//        tB1.solveTaskB1();
+
+	TaskB2 tB2 = new TaskB2();
+	tB2.calcY();
+    }
+}
--- src/by/it/mustaphin/jd01_06/TaskB2.java
+++ src/by/it/mustaphin/jd01_06/TaskB2.java
@@ -10,31 +10,31 @@ import java.util.regex.Pattern;
 class TaskB2 {
 
     void seekLengthParts(String text) {
-        Pattern pat = Pattern.compile("[\\.!]{1,}");
-        Matcher mat = pat.matcher(text);
-        while (mat.find()) {
-
-        }
+	Pattern pat = Pattern.compile("[\\.!]{1,}");
+	Matcher mat = pat.matcher(text);
+	while (mat.find()) {
+	    System.out.println(mat.start() + " : " + mat.end());
+	}
     }
 
     void seekLengthParts2(String text) {
-        String textMas[] = text.split("[\\.!]{1,}");
-        int lengthMas[] = new int[textMas.length];
-        Map<Integer, List<String>> allParts = new HashMap<>();
-        int numb = 1;
-        for (String textPart : textMas) {
-            String[] innerMas = textPart.split("[^а-яА-ЯёЁ]");
-            List<String> listWords = new ArrayList<>();
-            for (String singleWord : innerMas) {
-                listWords.add(singleWord);
-            }
-            allParts.put(numb++, listWords);
-        }
-        int placeNumb = 0;
-        for (Map.Entry<Integer, List<String>> entry : allParts.entrySet()) {
-            lengthMas[placeNumb] = entry.getValue().size();
-            placeNumb++;
-        }
+	String textMas[] = text.split("[\\.!]{1,}");
+	int lengthMas[] = new int[textMas.length];
+	Map<Integer, List<String>> allParts = new HashMap<>();
+	int numb = 1;
+	for (String textPart : textMas) {
+	    String[] innerMas = textPart.split("[^а-яА-ЯёЁ]");
+	    List<String> listWords = new ArrayList<>();
+	    for (String singleWord : innerMas) {
+		listWords.add(singleWord);
+	    }
+	    allParts.put(numb++, listWords);
+	}
+	int placeNumb = 0;
+	for (Map.Entry<Integer, List<String>> entry : allParts.entrySet()) {
+	    lengthMas[placeNumb] = entry.getValue().size();
+	    placeNumb++;
+	}
     }
 
 }
