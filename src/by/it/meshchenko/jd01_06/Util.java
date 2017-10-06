d1fb0661b75db49f53c4f6c22fbb844a2d402eae
AlehMeshchanka <teraomol@gmail.com>
2017-10-06 17:20:07 +0300

    jd01_06: TaskС .2

--- src/by/it/meshchenko/jd01_06/Util.java
+++ src/by/it/meshchenko/jd01_06/Util.java
@@ -1,5 +1,7 @@
 package by.it.meshchenko.jd01_06;
 
+import java.util.Random;
+
 public class Util {
     static void sortByLengthLines(StringBuilder[] arr){
         int startIndex = 0;
@@ -36,35 +38,7 @@ public class Util {
         doSort1(arr, cur +1, end);
     }
 
-    static void quickSort(int[] arr) {
-        int startIndex = 0;
-        int endIndex = arr.length - 1;
-        doSort(arr, startIndex, endIndex);
-    }
-
-    private static void doSort(int[] arr, int start, int end) {
-        if (start >= end)
-            return;
-        int i = start, j = end;
-        int cur = i - (i - j) / 2;
-        while (i < j) {
-            while (i < cur && (arr[i] <= arr[cur])) {
-                i++;
-            }
-            while (j > cur && (arr[cur] <= arr[j])) {
-                j--;
-            }
-            if (i < j) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                if (i == cur)
-                    cur = j;
-                else if (j == cur)
-                    cur = i;
-            }
-        }
-        doSort(arr, start, cur);
-        doSort(arr, cur +1, end);
+    static int randomInt(int n) {
+        return new Random().nextInt(n);
     }
 }
