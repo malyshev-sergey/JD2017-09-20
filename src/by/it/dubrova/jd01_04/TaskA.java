004017036eb72ea861e1e74ede8fc5d142c00644
Dubrova <maksim.dubrova@gmail.com>
2017-10-06 12:23:58 +0300

    не доделанное есть

--- src/by/it/dubrova/jd01_04/TaskA.java
+++ src/by/it/dubrova/jd01_04/TaskA.java
@@ -4,17 +4,18 @@ public class TaskA {
     public static void main(String[] args) {
      //   int[10][10] = createMultiplicationTable(10,10);  // таблица умножения 0 - 9
     }
-/*
+
     public static int[][] createMultiplicationTable(int x, int y){
         //Создать массив и заполнить
         int[][] mt = new int[x][y];
         for (int i = 0; i < y; i++){ // заполняем x
-            for (int j = 0; j < x; j++){
-
+            mt[i] = i;
+            for (int j = 0; j < x; j++){ // заполняем y
+                mt[i][j] = j;
             }
 
         }
 
     }
-   */
+
 }
