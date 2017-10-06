0a6174557cc8080d776c2a588e9a4df3a57900a5
Dubrova <maksim.dubrova@gmail.com>
2017-10-06 12:02:49 +0300

    123

---
+++ src/by/it/dubrova/jd01_04/TaskA.java
@@ -0,0 +1,13 @@
+package by.it.dubrova.jd01_04;
+
+public class TaskA {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][] createMultiplicationTable(int x, int y){
+        //Создать массив и заполнить
+        int[][] = new int[x][y];
+
+    }
+}
