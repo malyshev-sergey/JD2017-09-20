495c79b8b91c0d4221b5abc3aac2c2cc0c31c7a1
marat <marat@station-0>
2017-10-08 01:17:51 +0300

    Почти правильно сделано jd01_06 TaskB2

--- src/by/it/mustaphin/jd01_06/TaskB2.java
+++ src/by/it/mustaphin/jd01_06/TaskB2.java
@@ -6,14 +6,33 @@ import java.util.regex.Pattern;
 class TaskB2 {
 
     void seekLengthParts(String text) {
-	Pattern pat = Pattern.compile("[а-яА-ЯёЁ;: -]+[\\.!]{1,}");
-	Matcher mat = pat.matcher(text);
-	int part = 0;
-	while (mat.find()) {
-	    String words[] = mat.group().split("[^а-яА-ЯёЁ]");
-	    System.out.println(mat.group());
-	    System.out.printf("%-1d : %-3d\n", ++part, words.length);
+	StringBuilder sb = new StringBuilder();
+	Pattern patLine = Pattern.compile(".+\n|[а-яА-ЯёЁ ]+[\\.!]{1,}");
+	Pattern patPunc = Pattern.compile("[\\.!]{1,}");
+	Pattern patWord = Pattern.compile("[^а-яА-ЯёЁ:;,-]");
+	Matcher matLine = patLine.matcher(text);
+	while (matLine.find()) {
+	    sb.append(matLine.group().replace("\n", " "));
+	}
+	String senses[] = String.valueOf(sb).split(patPunc.pattern());// [\\.!]{1,}
+	String words[][] = new String[senses.length][];
+	for (int i = 0; i < senses.length; i++) {
+	    words[i] = senses[i].split(patWord.pattern());// [^а-яА-ЯёЁ:;-]
+	}
+	for (int i = words.length - 1; i > 0; i--) {
+	    for (int k = 0; k < i; k++) {
+		if (words[k].length > words[k + 1].length) {
+		    String swap[] = words[k];
+		    words[k] = words[k + 1];
+		    words[k + 1] = swap;
+		}
+	    }
+	}
+	for (int i = 0; i < words.length; i++) {
+	    for (int k = 0; k < words[i].length; k++) {
+		System.out.printf("%s%d ", words[i][k], k);
+	    }
+	    System.out.printf("%d\n", words[i].length);
 	}
     }
-
 }
