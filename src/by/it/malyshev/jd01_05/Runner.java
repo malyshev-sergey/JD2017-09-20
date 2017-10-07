8b12de1668adefe85424eebdf3403307a756cc7c
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-07 11:34:49 +0300

    05_TaskC2 solved 1.0

--- src/by/it/malyshev/jd01_05/Runner.java
+++ src/by/it/malyshev/jd01_05/Runner.java
@@ -7,9 +7,9 @@ public class Runner {
         System.out.println("\nЗадание№1\n");
         TaskA.solveTaskA1();
 
-//        System.out.println("\nЗадание№2\n");
-//        System.out.println("ввести a=1.21");
-//        TaskA.solveTaskA2();
+        System.out.println("\nЗадание№2\n");
+        System.out.println("ввести a=1.21");
+        TaskA.solveTaskA2();
 
         System.out.println("\nЗадание№3\n");
         TaskA.sovleTaskA3();
@@ -21,7 +21,11 @@ public class Runner {
         TaskB.functionFive(-2, 15, 1.5, 0.491E4);
 
         System.out.println("\nЗадание№6\n");
-        TaskC.functionSeven(5.33,9,3.5);
+        TaskC.functionSix(5.33,9,3.5);
+
+        System.out.println("\nЗадание№7\n");
+        TaskC.functionSeven(103,450,31,10,5);
+
 
     }
 }
