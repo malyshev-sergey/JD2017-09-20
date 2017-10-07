6f6929ad0cc4658cc0fced0038d4e195c42ab98b
Dubrova <maksim.dubrova@gmail.com>
2017-10-07 16:38:25 +0300

    таблица умножения

--- src/by/it/dubrova/jd01_04/TaskA.java
+++ src/by/it/dubrova/jd01_04/TaskA.java
@@ -3,19 +3,23 @@ package by.it.dubrova.jd01_04;
 public class TaskA {
     public static void main(String[] args) {
      //   int[10][10] = createMultiplicationTable(10,10);  // таблица умножения 0 - 9
+        int[][]  z = new int[20][20];
+        z = createMultiplicationTable(20,20);
     }
 
     public static int[][] createMultiplicationTable(int x, int y){
         //Создать массив и заполнить
         int[][] mt = new int[x][y];
         for (int i = 0; i < y; i++){ // заполняем x
-            mt[i] = i;
+            //mt[i] = i;
             for (int j = 0; j < x; j++){ // заполняем y
-                mt[i][j] = j;
+                mt[i][j] = i * j;
+                System.out.print(i + "*" + j + "=" + mt[i][j] + "\t");
             }
 
+            System.out.println();
         }
-
+        return mt;
     }
 
 }
