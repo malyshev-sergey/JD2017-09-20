e5c5ec656b2a1b65f89811e5dcce99e894428082
marat <marat@station-0>
2017-10-08 16:21:04 +0300

    Решение jd01_06 TaskB2

--- src/by/it/mustaphin/jd01_06/TaskB2.java
+++ src/by/it/mustaphin/jd01_06/TaskB2.java
@@ -1,42 +1,33 @@
 package by.it.mustaphin.jd01_06;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class TaskB2 {
 
     void seekLengthParts(String text) {
-	StringBuilder sb = new StringBuilder();
-	Pattern patLine = Pattern.compile(".+\n|[а-яА-ЯёЁ ]+[\\.!]{1,}");
-	Pattern patPunc = Pattern.compile("[\\.!]{1,}");
-	Pattern patWord = Pattern.compile("[^а-яА-ЯёЁ:;,-]");
-	Matcher matLine = patLine.matcher(text);
-	while (matLine.find()) {
-	    sb.append(matLine.group().replace("\n", " "));
+	List<Sense> allText = new ArrayList<>();
+	String oneLine = text.replace("\n", " ");
+	Pattern patPunct = Pattern.compile("[а-яА-ЯёЁ:,; -]+[\\.!]{1,}");
+	Matcher mat = patPunct.matcher(oneLine);
+	while (mat.find()) {
+	    allText.add(new Sense(mat.group()));
 	}
-	String senses[] = String.valueOf(sb).split(patPunc.pattern());// [\\.!]{1,}
-	String words[][] = new String[senses.length][];
-	for (int i = 0; i < senses.length; i++) {
-	    words[i] = senses[i].split(patWord.pattern());// [^а-яА-ЯёЁ:;-]
-	}
-	for (int i = words.length - 1; i >= 0; i--) {
-	    for (int k = 0; k < i; k++) {
-		if (words[i][0].equals(" ")) {
-//		    System.out.printf("символ: \"%s\"\n", words[i][0]);
-		    continue;
-		}
-		if (words[k].length > words[k + 1].length) {
-		    String swap[] = words[k];
-		    words[k] = words[k + 1];
-		    words[k + 1] = swap;
+	Sense[] senses = new Sense[allText.size()];
+	senses = allText.toArray(senses);
+	for (int i = senses.length - 1; i >= 0; i--) {
+	    for (int j = 0; j < i; j++) {
+		if (senses[j].wordsCount() > senses[j + 1].wordsCount()) {
+		    Sense swap = senses[j + 1];
+		    senses[j + 1] = senses[j];
+		    senses[j] = swap;
 		}
 	    }
 	}
-	for (int i = 0; i < words.length; i++) {
-	    for (int k = 0; k < words[i].length; k++) {
-		System.out.printf("%s%d ", words[i][k], k);
-	    }
-	    System.out.printf("%d\n", words[i].length);
+	for (Sense sense : senses) {
+	    System.out.println(sense.data);
 	}
     }
 }
