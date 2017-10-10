e6f2cca60e47b46cb9db8c44d6d21e5dcc0bf710
Dubrova <maksim.dubrova@gmail.com>
2017-10-10 17:41:28 +0300

    Задание Ц - вся директория

---
+++ src/by/it/dubrova/jd01_07/TaskC/IPerson.java
@@ -0,0 +1,11 @@
+package by.it.dubrova.jd01_07.TaskC;
+
+public interface IPerson {
+    void getName();
+    void getSurname();
+    void getAge();
+    void getSex();
+    void getAddress();
+    void getTelephoneNumber();
+    void getExpirence();
+}
