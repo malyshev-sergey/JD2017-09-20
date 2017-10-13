3fa7b111c4f2be53a39cd3032026c969aedca576
Ahremenko <a.hremenko@gmail.com>
2017-10-13 12:31:02 +0300

    Started calc

---
+++ src/by/it/ali/Calc/Var.java
@@ -0,0 +1,28 @@
+package by.it.ali.Calc;
+
+
+abstract class Var implements Func{
+    @Override
+    public Var add(Var x) {
+        System.out.println("Сложение невозможно");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var x) {
+        System.out.println("Вычитание невозможно");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var x) {
+        System.out.println("Умножение невозможно");
+        return null;
+    }
+
+    @Override
+    public Var div(Var x) {
+        System.out.println("Деление невозможно");
+        return null;
+    }
+}
