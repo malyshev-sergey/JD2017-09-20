e99897452eacd806cd11f4e65cf07b6f46d692aa
Ahremenko <a.hremenko@gmail.com>
2017-10-13 12:31:02 +0300

    Started calc

---
+++ src/by/it/ali/Calc/Func.java
@@ -0,0 +1,9 @@
+package by.it.ali.Calc;
+
+
+interface Func {
+    Var add(Var x);
+    Var sub(Var x);
+    Var mul(Var x);
+    Var div(Var x);
+}
---
+++ src/by/it/ali/Calc/VarD.java
@@ -0,0 +1,11 @@
+package by.it.ali.Calc;
+
+
+public class VarD extends Var implements Func{
+
+
+    @Override
+    public Var add(Var x){
+        return null;
+    }
+}
