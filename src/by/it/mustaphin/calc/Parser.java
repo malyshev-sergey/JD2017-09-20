b097e57b051ab7893908dbfbddf19744f1f54d61
marat <pcgeniusweb@gmail.com>
2017-10-16 19:16:13 +0300

    Начал делать calc.Parser

---
+++ src/by/it/mustaphin/calc/Parser.java
@@ -0,0 +1,16 @@
+package by.it.mustaphin.calc;
+
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    private String value;
+    Pattern numb = Pattern.compile("[.0-9]+");
+    Pattern vec = Pattern.compile("[{]");
+
+
+    void parse(String value) {
+
+    }
+
+}
