5b7d628cdb7fd293b65272fad191c8f784a5942b
marat <pcgeniusweb.mm@gmail.com>
2017-10-16 19:50:00 +0300

    На чал делать calc.Parser

--- src/by/it/mustaphin/calc/Parser.java
+++ src/by/it/mustaphin/calc/Parser.java
@@ -1,16 +1,27 @@
 package by.it.mustaphin.calc;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
 
     private String value;
-    Pattern numb = Pattern.compile("[.0-9]+");
-    Pattern vec = Pattern.compile("[{]");
+    Pattern patNumber = Pattern.compile("[.0-9]+");
+    Pattern patVector = Pattern.compile("[{]");
+    Pattern patMatrix = Pattern.compile("[{{]");
 
+    public Parser(String value) {
+        this.value = value;
+    }
 
     void parse(String value) {
+        Matcher matM = patMatrix.matcher(value);
+        Matcher matV = patVector.matcher(value);
+        if (matM.find()) {
 
+        }else if(matV.find()){
+
+        }
     }
 
 }
