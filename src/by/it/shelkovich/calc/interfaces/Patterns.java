98e775e1705980211f3258a4a3054cab9bbbcbb8
Anton Shelkovich <anton.shelkovich@gmail.com>
2017-10-18 11:37:33 +0300

    Исправление ошибки парсера калькулятора, когда он отрицательное значение считал за операцию

--- src/by/it/shelkovich/calc/interfaces/Patterns.java
+++ src/by/it/shelkovich/calc/interfaces/Patterns.java
@@ -2,13 +2,15 @@ package by.it.shelkovich.calc.interfaces;
 
 public interface Patterns {
     //возможные примеры регулярных выражений (конечно лучше составить свои).
+    String exName="(([A-Za-z0-9])+)";                       //имена переменных
     String exVal="((-?)([0-9.])+)";                       //числа
     String exVec="\\{((-?([0-9.])+),?)+}";                //вектора
     String exMat="\\{((\\{((-?([0-9.])+),?)+}),?)+}";     //матрицы
-    String exAny="("+exMat+")|("+exVec+")|("+exVal+")";   //одно из...
+    String exVarAny="("+exMat+")|("+exVec+")|("+exVal+")";   //одно из...
+    String exAny = "("+exMat+")|("+exVec+")|("+exVal+")|("+exName+")";
     String exOper="[-+*/=]";                               //операция
-    String exFull= "("+exAny+")"+
-                   "(" +exOper+")"+
-                   "(" +exAny +")"; //выражение целиком
+    String exFull= "[ ]?(?<first>"+exAny+")[ ]?"+
+                   "(?<op>" +exOper+")[ ]?"+
+                   "(?<second>" +exVarAny +")[ ]?"; //выражение целиком
 
 }
