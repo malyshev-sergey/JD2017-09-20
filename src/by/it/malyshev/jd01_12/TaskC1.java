a09eaa9a2dbc250277b2e950584a4aabfbe1299f
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-20 11:41:12 +0300

    Task12 C2

--- src/by/it/malyshev/jd01_12/TaskC1.java
+++ src/by/it/malyshev/jd01_12/TaskC1.java
@@ -2,5 +2,27 @@ package by.it.malyshev.jd01_12;
 
 public class TaskC1 {
 
-    
+    public static void main(String[] args) {
+
+
+    }
+
+   private static void inputTxtModelling() {
+//       StringBuilder sb = new StringBuilder(Data.TXT);
+//       int sbStartLength = sb.length();
+//
+//       Pattern space = Pattern.compile(" ");
+//       Matcher mSpace = space.matcher(sb);
+//       for (int i = 0; i < 20; i++) {
+//           while (!mSpace.find((int) (Math.random() * m.group().length()))) {
+//               mSpace.reset();
+//           }
+//           a.insert(mSpace.start(), " ");
+//       }
+//       sb.append(a.toString());
+//   }
+
+
+   }
+
 }
