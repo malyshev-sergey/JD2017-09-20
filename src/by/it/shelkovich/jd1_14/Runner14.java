43594fba6fc482d51c488c214381463095aeb586
Anton Shelkovich <anton.shelkovich@gmail.com>
2017-10-23 15:30:55 +0300

    оьщий внешний вид заданий 14го занятия

--- src/by/it/shelkovich/jd1_14/Runner14.java
+++ src/by/it/shelkovich/jd1_14/Runner14.java
@@ -2,8 +2,11 @@ package by.it.shelkovich.jd1_14;
 
 public class Runner14 {
     public static void main(String[] args) {
-        //TaskA.run();
-        //TaskB.run();
+        System.out.println("Задание А:");
+        TaskA.run();
+        System.out.println("\n\nЗадание B:");
+        TaskB.run();
+        System.out.println("\n\nЗадание C:");
         TaskC.run();
     }
 }
