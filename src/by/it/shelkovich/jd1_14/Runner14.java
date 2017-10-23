0b8b80ac9066dff9eabb324eb6b736a812869411
Anton Shelkovich <anton.shelkovich@gmail.com>
2017-10-23 15:27:42 +0300

    Задание C 14го занятия

--- src/by/it/shelkovich/jd1_14/Runner14.java
+++ src/by/it/shelkovich/jd1_14/Runner14.java
@@ -3,6 +3,7 @@ package by.it.shelkovich.jd1_14;
 public class Runner14 {
     public static void main(String[] args) {
         //TaskA.run();
-        TaskB.run();
+        //TaskB.run();
+        TaskC.run();
     }
 }
