185b712891059093d9c52006b08095e79cff33a2
Sergei <shishovserega@mail.ru>
2017-10-23 20:41:09 +0300

    jd01_12 TaskA done

---
+++ src/by/it/shishov/jd01_14/Runner.java
@@ -0,0 +1,12 @@
+package by.it.shishov.jd01_14;
+
+import java.io.IOException;
+
+/**
+ * Created by Сергей on 23.10.2017.
+ */
+public class Runner {
+    public static void main(String[] args) throws IOException {
+        TaskA.solveTaskA();
+    }
+}
