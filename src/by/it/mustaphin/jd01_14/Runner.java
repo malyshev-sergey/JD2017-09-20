9d360ffedeac8575829b8a4c71159f912b8bacfa
marat <pcgeniusweb.mm@gmail.com>
2017-10-23 19:49:24 +0300

    jd01_14 TaskB

--- src/by/it/mustaphin/jd01_14/Runner.java
+++ src/by/it/mustaphin/jd01_14/Runner.java
@@ -1,9 +1,14 @@
 package by.it.mustaphin.jd01_14;
 
+import java.io.File;
+
 public class Runner {
     public static void main(String[] args) {
 //        new TaskA().write();
 //        new TaskA().read();
-        new TaskB().count();
+//
+//        new TaskB().count();
+
+        new TaskC().printInfo(new File(System.getProperty("user.dir") + "/src/by/it/mustaphin/"));
     }
 }
---
+++ src/by/it/mustaphin/jd01_14/TaskC.java
@@ -0,0 +1,31 @@
+package by.it.mustaphin.jd01_14;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class TaskC {
+
+    Map<String, File> mapFiles = new HashMap<>();
+    Set<String> checked = new HashSet<>();
+    Map<String, File> mapDirs = new HashMap<>();
+
+    void printInfo(File file) {
+        File files[] = file.listFiles();
+        for (File inner : files) {
+            if (inner.isDirectory()) {
+                mapDirs.put(inner.getAbsolutePath(), inner);
+//                for () {
+//
+//                }
+            } else if (inner.isFile()) {
+                mapFiles.put(inner.getAbsolutePath(), inner);
+            }
+        }
+    }
+
+
+
+}
