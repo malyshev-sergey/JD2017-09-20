21392cb70b6c8186b46d8400c701b5f320abc107
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-24 00:36:39 +0300

    14 Task A,B

--- src/by/it/malyshev/jd01_14/TaskA.java
+++ src/by/it/malyshev/jd01_14/TaskA.java
@@ -6,18 +6,35 @@ public class TaskA {
 
     public static void main(String[] args) {
 
-        String src=System.getProperty("user.dir")+"/src/by/it/malyshev/";
-        String filename = src+"jd01_14/integer.dat";
-        File f=new File(filename);
+        String dir = System.getProperty("user.dir") + "/src/by/it/malyshev/jd01_14";
+        String filename = "integer.dat";
+        File f = new File(dir, filename);
 
-        try {DataOutputStream a=new DataOutputStream(new FileOutputStream(f));
+        try {
+            DataOutputStream out = new DataOutputStream(new FileOutputStream(f));
             for (int i = 0; i < 20; i++) {
-                a.writeInt((int)(Math.random()*20-10));
+                out.writeDouble((Math.random() * 50));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-    }
 
+        try (DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(f)))) {
+
+            double sum = 0;
+            double count = 0;
+            while (in.available() > 0) {
+                double d = in.readDouble();
+                System.out.printf("%4.2f\n", d);
+                sum += d;
+                count++;
+            }
+            System.out.printf("Среднее арифмтическое = %4.3f\n", sum / count);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
