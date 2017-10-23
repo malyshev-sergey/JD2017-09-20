9a1606886de4cfac7359bc3afe912869aa97790f
AlehMeshchanka <teraomol@gmail.com>
2017-10-23 11:41:28 +0300

    jd01_14 start

--- src/by/it/meshchenko/calc/Expression.java
+++ src/by/it/meshchenko/calc/Expression.java
@@ -3,6 +3,7 @@ package by.it.meshchenko.calc;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Stack;
 
 public class Expression {
     public LinkedHashMap<String, Var> tempEx;
@@ -15,5 +16,30 @@ public class Expression {
     public Expression(LinkedHashMap<String, Var> tempEx, ArrayList<String> tempOp) {
         this.tempEx = tempEx;
         this.tempOp = tempOp;
+    }
+
+    public boolean isBrackets(String str){
+        boolean res = false;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            switch (str.charAt(i)){
+                case '{': stack.push('{'); break;
+                case '(': stack.push('('); break;
+                case '[': stack.push('['); break;
+                case '}':
+                    if(!stack.isEmpty() && stack.pop().compareTo('{') == 0){ break; }
+                    else{ return res;}
+                case ')':
+                    if(!stack.isEmpty() && stack.pop().compareTo('(') == 0){ break; }
+                    else{ return res; }
+                case ']':
+                    if(!stack.isEmpty() && stack.pop().compareTo('[') == 0){ break; }
+                    else{ return res; }
+                default: break;
+            }
+
+        }
+        if(stack.isEmpty()) res = true;
+        return res;
     }
 }
