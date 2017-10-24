e2db2eabdaffaaaa1316a211a1c7314a891eeb2f
Dubrova <maksim.dubrova@gmail.com>
2017-10-25 00:35:14 +0300

    работа с потоками

--- src/by/it/dubrova/jd01_14/TaskA.java
+++ src/by/it/dubrova/jd01_14/TaskA.java
@@ -1,6 +1,7 @@
 package by.it.dubrova.jd01_14;
 
 import java.io.*;
+import java.util.*;
 
 public class TaskA {
 
@@ -31,10 +32,42 @@ public class TaskA {
         }
 
     }
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    public static void readIntsFromFile(String filename) throws IOException {
+        filename = System.getProperty("user.dir") + "/../../"+filename;
+        DataInputStream fis = null;
+        List<Integer> ints = new ArrayList<>();
+
+        try{
+        fis = new DataInputStream(new FileInputStream(new File(filename)));
+            do {
+                ints.add(fis.readInt());
+            } while (fis.available() != 0);
+        }
+        catch (FileNotFoundException fnf){
+            System.out.println("File Not Found!");
+        }
+        finally {
+            try{
+                fis.close();
+            }
+            catch (FileNotFoundException fnf){
+                System.out.println("Не получается закрыть файл");
+            }
+        }
+
+        System.out.println("Ints из файла: " + ints);
+        int tmp = 0;
+        for (int i = 0; i < 20; i++){
+            tmp += ints.get(i);
+        }
+        System.out.println("Среднее арифметическое: " + tmp / 20);
+
+    }
 
     public static void main(String[] args) throws IOException {
         writeIntsToFile("testtt1.txt");
-
+        readIntsFromFile("testtt1.txt");
 
     }
 
