65d47a079d72a2283a9c2a7f49cc94ac68622fdc
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-24 09:22:57 +0300

    14 Task A,B,C

--- src/by/it/malyshev/jd01_14/TaskC.java
+++ src/by/it/malyshev/jd01_14/TaskC.java
@@ -8,21 +8,21 @@ public class TaskC {
         String dir = System.getProperty("user.dir") + "/src/by/it/malyshev";
 
         System.out.println("\nСписок файлов");
-        getListFiles(dir, true);
+        getListFilesDirs(dir, true);
         System.out.println("\nСписок каталогов");
-        getListFiles(dir, false);
+        getListFilesDirs(dir, false);
 
     }
 
-    private static void getListFiles(String dir, boolean isFile) {
+    private static void getListFilesDirs(String dir, boolean needFile) {
         File[] files =(new File(dir)).listFiles();
         if (!(files==null)) {
             for (File s : files) {
                 if (s.isFile()) {
-                    if (isFile) System.out.println(s.getName());
+                    if (needFile) System.out.println(s.getName());
                 } else if (s.isDirectory()) {
-                    if (!isFile) System.out.println(s.getName());
-                    getListFiles(s.getPath(), isFile);
+                    if (!needFile) System.out.println(s.getName());
+                    getListFilesDirs(s.getPath(), needFile);
                 }
             }
         } else {
