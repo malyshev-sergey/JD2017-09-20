7de24bf508aa9374b4f48647863d3dac539caaa4
AliAbbas29 <AliAbbas2905@gmail.com>
2017-10-24 23:15:27 +0300

    jd01_12 Tryed B + jd01_13 Done A?

--- src/by/it/ali/jd01_13/TaskA.java
+++ src/by/it/ali/jd01_13/TaskA.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import static java.lang.Double.parseDouble;
 
 public class TaskA {
-    static void SolveTaskA()
+    static void SolveTaskA() //throws NumberFormatException, ArithmeticException//
     {
         try{
         String a="10";
@@ -16,14 +16,12 @@ public class TaskA {
         String c = sc.nextLine();
         System.out.println("Числа:"+a+" "+b+" "+c);
         double d = (Math.sqrt(parseDouble(a)) + Math.sqrt(parseDouble(b))+Math.sqrt(parseDouble(c)));
-        System.out.println("Сумма корней всех чисел: "+d);
-            if (parseDouble(c)<0){ throw new ArithmeticException();}
-        }
+        System.out.println("Сумма корней всех чисел: "+d);}
         catch (NumberFormatException e){
-            System.err.println("Неверно введено число! "+e);
+            System.out.println("Неверно введено число! "+e);
         }
         catch (ArithmeticException e){
-            System.err.println("Введено отрицательное число! "+e);
-        }
+            System.out.println("Введено отрицательное число! "+e);
         }
     }
+}
