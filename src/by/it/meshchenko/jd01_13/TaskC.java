3671fc43cc625232697c4d5f88e5de96f9388fa7
AlehMeshchanka <teraomol@gmail.com>
2017-10-24 18:39:09 +0300

    jd01_13 Task A,B,C

---
+++ src/by/it/meshchenko/jd01_13/TaskC.java
@@ -0,0 +1,7 @@
+package by.it.meshchenko.jd01_13;
+
+public class TaskC {
+    public static void example1() throws Exception {
+        NewBookCase newBookCase = new NewBookCase("NewCase", 1500, 1000, -1000, 10);
+    }
+}
