d159eda8c9cbf1894014b6f0e1e089f742ea93db
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-25 11:53:41 +0300

    15 TaskA

--- src/by/it/malyshev/jd01_15/TaskA.java
+++ src/by/it/malyshev/jd01_15/TaskA.java
@@ -2,5 +2,19 @@ package by.it.malyshev.jd01_15;
 
 public class TaskA {
 
+    public static void main(String[] args) {
 
-}
+        int[][] matrix = new int[4][4];
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                matrix[i][j]=(int)(Math.random()*31)-15;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++)
+                System.out.printf("%3d ", matrix[i][j]);
+            System.out.println();
+        }
+
+    } // main
+
+} // TaskA
