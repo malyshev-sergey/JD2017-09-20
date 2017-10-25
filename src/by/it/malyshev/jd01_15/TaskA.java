0abcdd27eb89db01e4c0bb8c81c89ca766d2a3e2
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-25 12:14:17 +0300

    15 TaskA

--- src/by/it/malyshev/jd01_15/TaskA.java
+++ src/by/it/malyshev/jd01_15/TaskA.java
@@ -1,5 +1,9 @@
 package by.it.malyshev.jd01_15;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class TaskA {
 
     public static void main(String[] args) {
@@ -13,6 +17,20 @@ public class TaskA {
             for (int j = 0; j < 4; j++)
                 System.out.printf("%3d ", matrix[i][j]);
             System.out.println();
+        }
+
+        String dir = System.getProperty("user.dir") + "/src/by/it/malyshev/jd01_15/matrix.txt";
+
+        try (PrintWriter prt=new PrintWriter(new FileWriter(dir,false))) {
+
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++)
+                    prt.printf("%3d ", matrix[i][j]);
+                prt.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     } // main
