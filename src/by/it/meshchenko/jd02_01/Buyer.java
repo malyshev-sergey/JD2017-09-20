27acb73bd2a3d4fc313c7388221908175fd7a204
AlehMeshchanka <teraomol@gmail.com>
2017-10-27 12:03:19 +0300

    jd02_01 start v.2

--- src/by/it/meshchenko/jd02_01/Buyer.java
+++ src/by/it/meshchenko/jd02_01/Buyer.java
@@ -1,4 +1,18 @@
 package by.it.meshchenko.jd02_01;
 
 public class Buyer extends Thread implements IBuyer {
+    @Override
+    public void enterToMarket() {
+
+    }
+
+    @Override
+    public void chooseGoods() {
+
+    }
+
+    @Override
+    public void goToOut() {
+
+    }
 }
