d9140853ec0085cc35309aa5dacee141258ec592
malyshev-sergey <malyshev.serge@gmail.com>
2017-10-29 01:24:21 +0300

    15 TaskA,B,C

--- src/by/it/malyshev/jd01_15/TaskC.java
+++ src/by/it/malyshev/jd01_15/TaskC.java
@@ -1,121 +1,191 @@
 package by.it.malyshev.jd01_15;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class TaskC {
 
+    private static Path p;
+
     public static void main(String[] args) throws IOException {
 
-        String src=System.getProperty("user.dir");
+        String path = System.getProperty("user.dir");
+        p = Paths.get(path);
+        System.out.print("\n"+p.toString() + " > ");
 
-        // Cоздание объекта Path через вызов статического метода get() класса Paths
-        Path d = Paths.get(src);
+        String inputLine;
 
+        label:
+        while (true) {
+            inputLine = getCommandLine();
+            List<String> parseInputLine = new ArrayList<>();
+            parseInputLine.addAll(Arrays.asList(inputLine.split(" ")));
+            parseInputLine.removeIf(s -> s.equals(""));
+            if (parseInputLine.size() > 2) {
+                System.out.print(p.toString() + " > ");
+                continue;
+            }
 
-        //Вывод информации о файле
-        System.out.println("Printing file information: ");
-        System.out.println("\t file name: " + d.getFileName());
-        System.out.println("\t root of the path: " + d.getRoot());
-        System.out.println("\t parent of the target: "
-                + d.getParent());
+            String command = parseInputLine.get(0).toLowerCase();
 
-        //Вывод элементов пути
-        System.out.println("Printing elements of the path: ");
-        for (Path element : d) {
-            System.out.println("\t path element: " + element);
+            switch (command) {
+                case "cd":
+                    cd(parseInputLine.get(1));
+                    break;
+                case "dir":
+                    dir();
+                    break;
+                case "exit":
+                    break label;
+            }
+            System.out.print(p.toString() + " > ");
         }
 
-        File f=new File(src);
-        File[] files=f.listFiles();
-        Arrays.sort(files);
+    } //main
 
-//        File[] files =(dir.new File(dir)).listFiles();
-//        if (files!=null) {
-//            for (File s : files) {
-//                if (s.isFile()) {
-//                    if (needFile) System.out.println(s.getName());
-//                } else if (s.isDirectory()) {
-//                    if (!needFile) System.out.println(s.getName());
-//                    getListFilesDirs(s.getPath(), needFile);
-//                }
-//            }
-//        } else {
-//            System.out.println("Нет такого каталога");
-//        }
+    private static String getCommandLine() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputLine = null;
+        try {
+            inputLine = br.readLine().trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return inputLine;
+    }
 
+    private static void cd(String newPath) {
+        Path temp=p;
+        try {
+            if (newPath.charAt(0) == '\\') {
+                temp = Paths.get(p.getRoot().toString() + newPath.substring(1));
+            } else if (newPath.length() > 1 && newPath.substring(0, 2).equals("..")) {
+                temp = Paths.get(p.getParent().toString() + newPath.substring(2));
+            } else if (newPath.contains(":\\")) {
+                temp = Paths.get(newPath);
+            } else if (newPath.equals(".")) {
+                temp=p;
+            } else if (newPath.length() > 1 && newPath.substring(0, 2).equals(".\\")) {
+                temp = Paths.get(p.toString() + newPath.substring(1));
+            } else
+                temp = Paths.get(p.toString() + "\\" + newPath);
+        } catch(InvalidPathException ignored) {}
+        if (Files.exists(temp)) {
+            p = temp;
+        }
+    }
 
+    private static void dir() {
 
-
-//        src = System.getProperty("user.dir") + "/src/by/it/akhmelev/";
-//        fileInTxt = src + "jd01_15/in.txt";
-//        String fileBak = src + "jd01_15/in.bak";
-//        String fileMove = src + "jd01_15/in.move";
-//        // Cоздание объекта Path через вызов статического метода get() класса Paths
-//        Path path = Paths.get(fileInTxt);
-//        Path pathForCopy= Paths.get(fileBak);
-//        Path pathForMove= Paths.get(fileMove);
-//        //проверка свойств файла (каталога)
-//        System.out.printf("Readable: %b, Writable: %b, Executable: %b\n",
-//                Files.isReadable(path), Files.isWritable(path),
-//                Files.isExecutable(path));
-//        //проверка аттрибутов файла (каталога)
-//        System.out.printf("Creation time: %s\nLast modified time: %s\nSize: %s\nDir: %s\n",
-//                Files.getAttribute(path, "creationTime", LinkOption.NOFOLLOW_LINKS),
-//                Files.getAttribute(path, "lastModifiedTime", LinkOption.NOFOLLOW_LINKS),
-//                Files.getAttribute(path, "size"),
-//                Files.getAttribute(path, "isDirectory")
-//        );
-//        //копирование файла(ов) (StandardCopyOption.REPLACE_EXISTING - перезапись)
-//        Files.copy(path,pathForCopy, StandardCopyOption.REPLACE_EXISTING);
-//        //перенос файла(ов) (StandardCopyOption.REPLACE_EXISTING - перезапись)
-//        Files.move(pathForCopy, pathForMove, StandardCopyOption.REPLACE_EXISTING);
-//        //удаление файла(ов)
-//        Files.delete(pathForMove);
-        int counterFiles=0;
-        int counterFolders=0;
-
+        File f = new File(p.toString());
+        File[] files = f.listFiles();
+        if (files!=null) {Arrays.sort(files);}
+        int counterFiles = 0;
+        int counterFolders = 0;
+        long length = 0;
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy KK:mm:ss");
-        System.out.println("Содержимое папки "+d.toAbsolutePath()+"\n");
-        for (File s : files) {
-            Path dir=Paths.get(s.getPath());
-            String isDir;
-            if (Files.getAttribute(dir, "isDirectory").equals(true)) {
-                isDir=" <dir> ";
-                counterFiles++;
-            }
-            else {
-                isDir="       ";
-                counterFolders++;
-            }
-            System.out.printf(" %s %s %10d  %s\n",
-                    dateFormat.format(Files.getLastModifiedTime(dir).toMillis()), isDir,
-                    Files.size(dir),/*getAttribute(dir, "size"),*/
-                    s.getName());
+
+        FileSystemView view = FileSystemView.getFileSystemView();
+        System.out.print ("\nТом в устройстве "+p.getRoot().toString().substring(0,1).toUpperCase());
+        String volumeLabel =view.getSystemDisplayName(new File(p.getRoot().toString()));
+        int index = volumeLabel.lastIndexOf(" (");
+        if (index > 0) {
+            volumeLabel = volumeLabel.substring(0, index);
         }
-        System.out.println("                  "+counterFiles+" файлов");
-        System.out.println("                  "+counterFolders+" папок");
-//
-//
-//        System.out.println();
-//        FileSystemView view = FileSystemView.getFileSystemView();
-//        Collection<String> logicalDrives = .getLogicalDriveStrings();
-//
-//        Kernel32Util
-//
+        if (volumeLabel.equals("Локальный диск")) System.out.println(" не имеет метки");
+        else System.out.println(" имеет метку "+volumeLabel);
+
+        if (System.getProperty("os.name").toLowerCase().contains("windows"))
+        System.out.println("Серийный номер тома: "+getVolumeSerialId());
+
+        System.out.println("\nСодержимое папки " + p.toAbsolutePath() + "\n");
+        try {
+            if (files!=null) {
+                for (File s : files) {
+                    Path dir = Paths.get(s.getPath());
+                    String isDir;
+                    if (Files.getAttribute(dir, "isDirectory").equals(true)) {
+                        isDir = " <dir> ";
+                        counterFolders++;
+                    } else {
+                        isDir = "       ";
+                        counterFiles++;
+                        length += s.length();
+                    }
+                    System.out.printf(" %s %s %10d  %s\n",
+                            dateFormat.format(Files.getLastModifiedTime(dir).toMillis()), isDir,
+                            Files.size(dir),
+                            s.getName());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("           %5d %7s %14d байт\r\n",
+                counterFiles, gramaticalCase(counterFiles, true), length);
+        System.out.printf("           %5d %7s %14d байт свободно\r\n",
+                counterFolders,gramaticalCase(counterFolders, false),f.getUsableSpace());
+
+        System.out.println();
+
+
+    }
+
+    private static String getVolumeSerialId() {
+
+        try {
+            Process pr = Runtime.getRuntime().exec("cmd /C dir "+p.getRoot().toString());
+            BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            String line;
+            int count = 0;
+            while ((line = in.readLine()) != null) {
+                count++;
+                if (count < 2)
+                    continue;
+                if (count > 2)
+                    break;
+                return line.substring(line.lastIndexOf(' ') + 1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String gramaticalCase(int q, boolean isFiles) {
+        String res;
+        int a = q;
+        while (a >= 100) a -= 100;
+        a /= 10;
+        int b = q % 10;
+        if ((a == 0 || a > 1) && (b == 2 || b == 3 || b == 4)) {
+            if (isFiles) res = "файла ";
+            else res = "папки ";
+        } else if ((a == 0 || a > 1) && (b == 1)) {
+            if (isFiles) res = "файл  ";
+            else res = "папка ";
+        } else {
+            if (isFiles) res = "файлов ";
+            else res = "папок  ";
+        }
+        return res;
     }
 
 
-
-
-
-}
+} //TaskC
 
 
 
