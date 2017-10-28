9d95c1ea854f508ca7812bc335b28680ee731c89
Sergei <shishovserega@mail.ru>
2017-10-28 17:42:05 +0300

    jd01_12 TaskB1 done

--- src/by/it/shishov/jd01_12/Runner.java
+++ src/by/it/shishov/jd01_12/Runner.java
@@ -10,13 +10,14 @@ import java.util.Set;
  */
 public class Runner {
     public static void main(String[] args) {
-        /*//ЗАДАНИЕ А1
+        //ЗАДАНИЕ А1
         System.out.println("Задание А1. Оценки до:");
         ArrayList<Integer> marks = TaskA.createListOfMarks();
         TaskA.printCollection(marks);
         TaskA.deleteBadMarks(marks);
         System.out.println("Оценки после: ");
         TaskA.printCollection(marks);
+
         //ЗАДАНИЕ А2
         Set<Integer> set1 = TaskA.createSetOfNumbers();
         System.out.println("Задание А2. Множество А: ");
@@ -28,14 +29,16 @@ public class Runner {
         TaskA.printCollection(MyCollect.getCross(set1,set2));
         System.out.println("Объединение множеств");
         TaskA.printCollection(MyCollect.getUnion(set1,set2));
+
         //ЗАДАНИЕ А3
         System.out.println("Задание А3. Множество до сортировки:");
         List<Integer> list = TaskA.createListOfNumbers();
         TaskA.printCollection(list);
         TaskA.sortCollection(list);
         System.out.println("Множество после сортировки: ");
-        TaskA.printCollection(list);*/
+        TaskA.printCollection(list);
 
+        //ЗАДАНИЕ B1
         TaskB.countWords();
 
     }
