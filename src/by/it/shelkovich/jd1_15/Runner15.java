4559de491673cff18890cf0d1a81ef7d8b3bed8a
Anton Shelkovich <anton.shelkovich@gmail.com>
2017-10-28 11:08:50 +0300

    Внешний вид заданий 15го занятия

--- src/by/it/shelkovich/jd1_15/Runner15.java
+++ src/by/it/shelkovich/jd1_15/Runner15.java
@@ -2,8 +2,11 @@ package by.it.shelkovich.jd1_15;
 
 public class Runner15 {
     public static void main(String[] args) {
-        //TaskA.run();
-        //TaskB.run();
+        System.out.println("Задание А:\n");
+        TaskA.run();
+        System.out.println("\n\nЗадание B:\n");
+        TaskB.run();
+        System.out.println("\n\nЗадание C:\n");
         TaskC.run();
     }
 }
