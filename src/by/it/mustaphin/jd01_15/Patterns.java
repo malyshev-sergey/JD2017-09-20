917eccf3f0d5b7636f5cd9547cd0c86e7d0b867a
marat <pcgeniusweb.mm@gmail.com>
2017-10-30 19:48:43 +0300

    jd01_15 TaskB

--- src/by/it/mustaphin/jd01_15/Patterns.java
+++ src/by/it/mustaphin/jd01_15/Patterns.java
@@ -2,7 +2,7 @@ package by.it.mustaphin.jd01_15;
 
 public class Patterns {
 
-    final static String patLineComent = "//.+\\n";
-    final static String patMultiComent = "/[*]+[a-zA-Z0-9()=!. \\n{};]+[*]/";
+    final static String patLineComent = "//.+";
+    final static String patMultiComent = "/[*]+[a-zA-Z0-9()=!.\\s\\n{};]+[*]/";
 
 }
