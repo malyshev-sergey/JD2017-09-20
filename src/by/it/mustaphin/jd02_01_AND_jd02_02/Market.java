cb875a2371a69a507eca30b72dc99bfce1d83984
marat223 <pcgeniusweb.mm@gmail.com>
2017-11-07 21:22:09 +0300

    jd02_02 TaskB

--- src/by/it/mustaphin/jd02_01_AND_jd02_02/Market.java
+++ src/by/it/mustaphin/jd02_01_AND_jd02_02/Market.java
@@ -1,6 +1,8 @@
 package by.it.mustaphin.jd02_01_AND_jd02_02;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 
 class Market implements Runnable {
 
@@ -10,7 +12,8 @@ class Market implements Runnable {
         letIn();
     }
 
-    static ConcurrentLinkedQueue<Buyer> buyers = new ConcurrentLinkedQueue<>();
+    static LinkedBlockingQueue<Buyer> buyers = new LinkedBlockingQueue<>(30);
+    List<Cashier> cashiers = new ArrayList<>();
 
     void letIn() {
         for (int i = 0; i < 60; i++) {
@@ -27,6 +30,45 @@ class Market implements Runnable {
 
     @Override
     public void run() {
-        Cashier c1 = new Cashier("Кассир 1");
+        for (int i = 0; i < 5; i++) {
+            cashiers.add(new Cashier("Кассир " + (i + 1)));
+        }
+        while (true) {
+            if (buyers.size() > 0) {
+                synchronized (cashiers.get(0)) {
+                    cashiers.get(0).notify();
+                }
+            } else {
+                cashiers.get(0).canClose = true;
+            }
+            if (buyers.size() > 5) {
+                synchronized (cashiers.get(1)) {
+                    cashiers.get(1).notify();
+                }
+            } else {
+                cashiers.get(1).canClose = true;
+            }
+            if (buyers.size() > 10) {
+                synchronized (cashiers.get(2)) {
+                    cashiers.get(2).notify();
+                }
+            } else {
+                cashiers.get(2).canClose = true;
+            }
+            if (buyers.size() > 15) {
+                synchronized (cashiers.get(3)) {
+                    cashiers.get(3).notify();
+                }
+            } else {
+                cashiers.get(3).canClose = true;
+            }
+            if (buyers.size() > 20) {
+                synchronized (cashiers.get(4)) {
+                    cashiers.get(4).notify();
+                }
+            } else {
+                cashiers.get(4).canClose = true;
+            }
+        }
     }
 }
