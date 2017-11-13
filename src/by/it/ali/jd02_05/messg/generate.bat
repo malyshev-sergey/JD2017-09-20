7092f963011338c3806e6461835fda045d3ad317
Ahremenko <a.hremenko@gmail.com>
2017-11-13 12:32:07 +0300

    jd02_05 A+B

---
+++ src/by/it/ali/jd02_05/messg/generate.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Java\jdk1.7.0_80\bin\native2ascii.exe" -encoding utf-8 message.txt ..\res\message.properties
+"C:\Program Files\Java\jdk1.7.0_80\bin\native2ascii.exe" -encoding utf-8 message_ru_RU.txt ..\res\message_ru_RU.properties
+"C:\Program Files\Java\jdk1.7.0_80\bin\native2ascii.exe" -encoding utf-8 message_be_BY.txt ..\res\message_be_BY.properties
