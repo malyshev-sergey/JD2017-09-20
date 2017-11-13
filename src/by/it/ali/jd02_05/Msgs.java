f17ee3d731f65dcf1c852c4d07245b3a4eac756a
Ahremenko <a.hremenko@gmail.com>
2017-11-13 12:32:07 +0300

    jd02_05 A+B

---
+++ src/by/it/ali/jd02_05/Msgs.java
@@ -0,0 +1,7 @@
+package by.it.ali.jd02_05;
+
+public interface Msgs {
+    String WELCOME="msg.welcome";
+    String NAME="msg.name";
+    String HRU="msg.hru";
+}
---
+++ src/by/it/ali/jd02_05/Runner.java
@@ -0,0 +1,25 @@
+package by.it.ali.jd02_05;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Runner {
+    public static void main(String[] args) {
+        if (args.length>=2){
+            String lang=args[0];
+            String country=args[1];
+            Locale locale = new Locale(lang,country);
+            ResMng.INSTANCE.SetLocale(locale);
+        }
+        else
+        {
+            Locale locale = Locale.US;
+            ResMng.INSTANCE.SetLocale(locale);
+        }
+
+        System.out.println(ResMng.INSTANCE.GetString("msg.welcome"));
+        System.out.println(ResMng.INSTANCE.GetString("msg.name"));
+        System.out.println(ResMng.INSTANCE.GetString("msg.hru"));
+    }
+}
