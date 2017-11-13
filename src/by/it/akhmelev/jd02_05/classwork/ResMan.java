21c626d272912534123e6e98dc10b737385f9543
ol smol <olga.smolyakova.work@gmail.com>
2017-11-13 12:44:19 +0300

    jd02_05 i18n classwork 2

--- src/by/it/akhmelev/jd02_05/classwork/ResMan.java
+++ src/by/it/akhmelev/jd02_05/classwork/ResMan.java
@@ -8,6 +8,7 @@ enum ResMan {
     private ResourceBundle bundle;
     private String Path="by.it.akhmelev.jd02_05.classwork.res.messages";
     ResMan() {
+        Locale.setDefault(Locale.ENGLISH);
         bundle = ResourceBundle.getBundle(Path,Locale.CANADA);
     }
     void setLocale(Locale locale){
