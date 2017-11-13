4d0d1b4f26cd2fece1595532f180891db25d0233
Ahremenko <a.hremenko@gmail.com>
2017-11-13 12:32:07 +0300

    jd02_05 A+B

---
+++ src/by/it/ali/jd02_05/Read.java
@@ -0,0 +1,14 @@
+//package by.it.ali.jd02_05;
+//
+//import java.util.Scanner;
+//
+//public class Read {
+//    Scanner sc = new Scanner(System.in);
+//
+//    void str(){
+//        System.out.println("Какую локаль использовать? RU, BY, EN ?");
+//        String locale = sc.nextLine();
+//        switch (locale)
+//            case:RU
+//    }
+//}
