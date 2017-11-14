57c9243a5db67f0084ed64803dfb9134f0b2b84c
AliAbbas29 <AliAbbas2905@gmail.com>
2017-11-14 20:29:52 +0300

    jd02_04 A.. jd02_05 A-C

--- src/by/it/ali/jd02_05/Read.java
+++ src/by/it/ali/jd02_05/Read.java
@@ -1,14 +1,28 @@
-//package by.it.ali.jd02_05;
-//
-//import java.util.Scanner;
-//
-//public class Read {
-//    Scanner sc = new Scanner(System.in);
-//
-//    void str(){
-//        System.out.println("Какую локаль использовать? RU, BY, EN ?");
-//        String locale = sc.nextLine();
-//        switch (locale)
-//            case:RU
-//    }
-//}
+package by.it.ali.jd02_05;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+class Read {
+    private Scanner sc = new Scanner(System.in);
+    Locale locale = Locale.getDefault();
+    protected Locale str() {
+        System.out.println("Какую локаль использовать? RU, BY, EN ?");
+        String strLocale = sc.nextLine();
+        switch (strLocale) {
+            case "RU":
+                locale = new Locale("ru", "RU");
+                break;
+            case "BY":
+                locale = new Locale("be", "BY");
+                break;
+            case "EN":
+                locale = Locale.US;
+                break;
+            default:
+                locale = new Locale("be", "BY");
+                break;
+        }
+        return locale;
+    }
+}
--- src/by/it/ali/jd02_05/Runner.java
+++ src/by/it/ali/jd02_05/Runner.java
@@ -5,18 +5,22 @@ import java.util.Locale;
 
 public class Runner {
     public static void main(String[] args) {
-        if (args.length>=2){
-            String lang=args[0];
-            String country=args[1];
-            Locale locale = new Locale(lang,country);
-            ResMng.INSTANCE.SetLocale(locale);
-        }
-        else
-        {
-            Locale locale = Locale.US;
-            ResMng.INSTANCE.SetLocale(locale);
-        }
-
+        Locale.setDefault(Locale.US);
+//        if (args.length>=2){
+//            String lang=args[0];
+//            String country=args[1];
+//            Locale locale = new Locale(lang,country);
+//            ResMng.INSTANCE.SetLocale(locale);
+//        }
+//        else
+//        {
+//            Locale locale = Locale.US;
+//            ResMng.INSTANCE.SetLocale(locale);
+//        }
+        Read r = new Read();
+        r.str();
+        ResMng.INSTANCE.SetLocale(r.locale);
+        System.out.println("r "+r.locale);
         System.out.println(ResMng.INSTANCE.GetString("msg.welcome"));
         System.out.println(ResMng.INSTANCE.GetString("msg.name"));
         System.out.println(ResMng.INSTANCE.GetString("msg.hru"));
