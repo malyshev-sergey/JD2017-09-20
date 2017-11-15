752e563cb41745a3e680b3f16472fa53424838df
Ahremenko <a.hremenko@gmail.com>
2017-11-15 12:08:06 +0300

    Edited junit (02_04)

--- src/by/it/ali/jd02_04/ParserTest.java
+++ src/by/it/ali/jd02_04/ParserTest.java
@@ -2,7 +2,7 @@ package by.it.ali.jd02_04;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ParserTest {
     @Test
@@ -10,8 +10,8 @@ public class ParserTest {
         Parser parser=new Parser();
         String expression="2+9";
         String res=parser.pars(expression).toString();
-        String control="8.0";
-        assertTrue(res.equals(control));
+        //String control="8.0";
+        //assertTrue(res.equals(control));
+        assertEquals("8",res);
     }
-
 }
