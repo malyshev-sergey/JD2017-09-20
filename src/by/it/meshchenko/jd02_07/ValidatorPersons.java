5171410c93f57235f70f997887368b3bbf9b8c6b
AlehMeshchanka <teraomol@gmail.com>
2017-11-18 17:03:45 +0300

    jd02_07 A,B,C ready

--- src/by/it/meshchenko/jd02_07/ValidatorPersons.java
+++
@@ -1,8 +0,0 @@
-package by.it.meshchenko.jd02_07;
-
-public class ValidatorPersons {
-    public static void main(String[] args){
-        String lang = javax.xml.XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI;
-        String filename = "";
-    }
-}
