ca698e754ae196d90d8dbdba884273860bc68fc3
marat223 <pcgeniusweb.mm@gmail.com>
2017-11-25 20:42:34 +0300

    Доделал многие предыдущие задания. В jd03_01 sql скрипт по удалению и созданию таблиц неправильно работает из IDEA

--- src/by/it/mustaphin/jd02_09/JAXB/Answers.java
+++ src/by/it/mustaphin/jd02_09/JAXB/Answers.java
@@ -1,22 +1,30 @@
 package by.it.mustaphin.jd02_09.JAXB;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
 
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlSeeAlso({Answer.class})
 @XmlType(name = "Answers", propOrder = "answers")
 public class Answers {
 
     @XmlElement(name = "answer")
     private ArrayList<Answer> answers = new ArrayList<Answer>();
 
-    public ArrayList<Answer> getAnswers() {return answers;}
+    public void add(Answer answer) {
+        answers.add(answer);
+    }
 
-    public void setAnswers(ArrayList<Answer> answers) {this.answers = answers;}
+    public ArrayList<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(ArrayList<Answer> answers) {
+        this.answers = answers;
+    }
 
     @Override
-    public String toString() {return "Answers: [" + answers + "]";}
+    public String toString() {
+        return "Answers: [" + answers + "]";
+    }
 }
--- src/by/it/mustaphin/jd02_09/JAXB/Tickets.java
+++ src/by/it/mustaphin/jd02_09/JAXB/Tickets.java
@@ -1,13 +1,13 @@
 package by.it.mustaphin.jd02_09.JAXB;
 
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.XMLConstants;
+import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
 
-@XmlRootElement
-@XmlType(name = "Tickets", propOrder = {"headers", "tickets"})
+@XmlRootElement(name="tickets")
+@XmlSeeAlso({Ticket.class, Header.class})
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "tickets", propOrder = {"headers", "tickets"})
 public class Tickets {
 
     @XmlAttribute(name = "W")
@@ -17,13 +17,21 @@ public class Tickets {
     @XmlElement(name = "ticket")
     private ArrayList<Ticket> tickets = new ArrayList<Ticket>();
 
-    public Tickets() {super();}
+    public Tickets() {
+        super();
+    }
 
-    public void setW(String w) {this.w = w;}
+    public void setW(String w) {
+        this.w = w;
+    }
 
-    public void setHeaders(ArrayList<Header> headers) {this.headers = headers;}
+    public void setHeaders(ArrayList<Header> headers) {
+        this.headers = headers;
+    }
 
-    public boolean addHeader(Header header) {return headers.add(header);}
+    public boolean addHeader(Header header) {
+        return headers.add(header);
+    }
 
     public void setTickets(ArrayList<Ticket> tickets) {
         this.tickets = tickets;
@@ -34,5 +42,7 @@ public class Tickets {
     }
 
     @Override
-    public String toString() {return "Header [headers=" + headers + "]\nTickets W=" + w + " [tickets=" + tickets + "]";}
+    public String toString() {
+        return "Header [headers=" + headers + "]\nTickets W=" + w + " [tickets=" + tickets + "]";
+    }
 }
