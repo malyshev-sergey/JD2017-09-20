6fc94614c2cfe31ea78dedffe5ef288f07086436
AliAbbas29 <AliAbbas2905@gmail.com>
2017-11-29 20:30:58 +0300

    jd03_01 C

--- src/by/it/ali/jd03_01/Ex_AdditionalRoles.java
+++ src/by/it/ali/jd03_01/Ex_AdditionalRoles.java
@@ -4,14 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
- class Ex_AdditionalRoles {
-  static void done() {
+
+public class Ex_AdditionalRoles {
+    public static void done(){
         try (
                 Connection connection = DriverManager.getConnection(INF.URL, INF.USER, INF.PASSWORD);
                 Statement statement=connection.createStatement();)
         {
-
-            statement.executeUpdate("insert into users(Login,Password,Email,FKroles, FKroles_additional)"+"values('Sarah','Moonie','SarahMoon@gmail.com','1','1');"
+//            statement.executeUpdate("insert into users(FKroles)"+"values('3');"
+//            );
+//            statement.executeUpdate("insert into users(FKroles)"+"values('3');"
+//            );
+            statement.executeUpdate("insert into users(Login,Password,Email,FKroles, FKroles_additional)"+"values('Sarah','qwerty12','SarahMoll@gmail.com','1','1');"
             );
             statement.executeUpdate("insert into users(Login,Password,Email,FKroles, FKroles_additional)"+"values('Donald','biggie','DonaldD@gmail.com','2','2');"
             );
@@ -19,6 +23,8 @@ import java.sql.Statement;
             );
             statement.executeUpdate("insert into users(FKroles)"+"values('3');"
             );
+//            statement.executeUpdate("insert into users(Login,Password,Email,FKroles)"+"values('Sarah','qwerty12','SarahMoll@gmail.com','2');"
+//            );
         } catch (SQLException e) {
             e.printStackTrace();
         }
