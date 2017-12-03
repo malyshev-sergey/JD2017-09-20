f4640a5128f20307248fa9304e198151b7b5a87b
marat223 <pcgeniusweb.mm@gmail.com>
2017-12-04 01:31:30 +0300

    project

---
+++ src/by/it/mustaphin/project/java/controller/CmdError.java
@@ -0,0 +1,8 @@
+package by.it.mustaphin.project.java.controller;
+
+public class CmdError extends AbstractAction {
+    @Override
+    public ICommand execute() {
+        return null;
+    }
+}
---
+++ src/by/it/mustaphin/project/java/controller/CmdSignup.java
@@ -0,0 +1,8 @@
+package by.it.mustaphin.project.java.controller;
+
+public class CmdSignup extends AbstractAction {
+    @Override
+    public ICommand execute() {
+        return null;
+    }
+}
