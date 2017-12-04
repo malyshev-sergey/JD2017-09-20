ac2cbedbcbe8466a22edbcb2cc83913042a31b0a
marat223 <09040006plomb>
2017-12-04 10:05:26 +0300

    jd03_03

--- src/by/it/mustaphin/jd03_01/CN.java
+++ src/by/it/mustaphin/jd03_01/CN.java
@@ -1,7 +1,7 @@
 package by.it.mustaphin.jd03_01;
 
 public interface CN {
-    String URL_DB = "jdbc:mysql://127.0.0.1:2016/mustaphin?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&useSSL=false";
+    String URL_DB = "jdbc:mysql://127.0.0.1:2016/?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&useSSL=false";
     String USER_DB = "root";
     String PASSWORD_DB = "";
 }
--- src/by/it/mustaphin/jd03_01/Runner.java
+++ src/by/it/mustaphin/jd03_01/Runner.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 public class Runner {
     public static void main(String[] args) {
         Connection con = new CreateConnection().connection;
-//        new C_Init(con).initDB();
         new A_AddUser(con).addUser();
         new A_AddData(con).updateCurrentPosition(2, 1);
         new A_AddData(con).updateCurrentPosition(2, 2);
