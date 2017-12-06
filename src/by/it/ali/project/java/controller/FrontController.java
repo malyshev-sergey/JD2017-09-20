72dc7f503443539d1e88cc1587b119d0868baf06
AliAbbas29 <AliAbbas2905@gmail.com>
2017-12-06 20:03:40 +0300

    jd03_05 (Servlets) Might done C?

--- src/by/it/ali/project/java/controller/FrontController.java
+++ src/by/it/ali/project/java/controller/FrontController.java
@@ -21,6 +21,18 @@ public class FrontController extends HttpServlet{
     private void processRequest(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException  {
         ICommand cmd=new ActionFactory().getCommand(req);
         ICommand nextAction=cmd.execute();
+        // Set to expire far in the past.
+        resp.setHeader("Expires", "Sat, 6 May 1995 12:00:00 GMT");
+
+        // Set standard HTTP/1.1 no-cache headers.
+        resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+
+        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+        resp.addHeader("Cache-Control", "post-check=0, pre-check=0");
+
+        // Set standard HTTP/1.0 no-cache header.
+        resp.setHeader("Pragma", "no-cache");
+
         if (nextAction==null){
         RequestDispatcher disp = req.getRequestDispatcher(cmd.getJsp());
         disp.include(req,resp);}
